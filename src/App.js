5aedeccfe285c87eabe220610880eb46a67c72b0
InnaMcGrane <innamcgrane@gmail.com>
2024-07-01 08:35:17 +0100

    my commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import Cart from "./Components/Cart/Cart";
+import Dishes from "./Components/DishesComponents/Dishes";
+import AllCategories from "./Filter/AllCategories";
+
+import Filter from "./Filter/Filter";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="block">
+        <AllCategories />
+
+        <Cart />
+        <Filter />
+      </div>
+      <div className="block">
+        <Dishes />
+      </div>
+    </div>
+  );
+}
+
+export default App;
